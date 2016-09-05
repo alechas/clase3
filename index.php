cfb07ee2212632b59d4e3cda09f5aafbb1bde05c
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-05 20:16:59 -0300

    a

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<link rel="stylesheet" type="text/css" href="animacion.css">
 </head>
 
-<div class="CajaInicio animated BouceIn>
+<div class="CajaInicio Animated BouceIn>
 <form action="destino.php" method="post" id="FormIngreso">
 <label> nombre </label>
 <input type = "text" name = "nombre">
