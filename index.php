2d8138fabdbe4d250cad893de13178c2d70c5000
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-05 20:20:38 -0300

    flash

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<link rel="stylesheet" type="text/css" href="animacion.css">
 </head>
 
-<div class="CajaInicio animated bounce">
+<div class="CajaInicio animated flash">
 <form action="destino.php" method="post" id="FormIngreso">
 <label> nombre </label>
 <input type = "text" name = "nombre">
